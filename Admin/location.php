a8b40c18a082aa036e6369a3893bc657f0731851
Ansel <anselhartanto@gmail.com>
2019-06-08 16:03:21 -0700

    location and locationView first pass, menu enhancing

---
+++ Admin/location.php
@@ -0,0 +1,319 @@
+
+
+<p><font size="20">AMS Location Database</p>
+<a href="locationView.php"><font size= "1.5">Click Here to see student's view</a><br/>
+<a href="main.php"><font size= "1.5">Back to Main Menu</a>
+
+<form method="POST" action="location.php"> 
+   <p><input type="submit" value="Initialize" name="reset"></p>
+</form>
+
+<form method="POST" action="location.php"> 
+   <p><input type="text" placeholder="type location ID here.." name="locationSearchString" size="18">
+   <input type="submit" value="Search for a location by its ID here" name="locationSearch"></p>
+</form>
+
+<form id="s" method="post" action="location.php">
+   <select name="updateValue">
+    <option value="locationID">Location ID</option>
+    <option value="buidingCode">Building Code</option>
+    <option value="areaCode">Area Code</option>
+    <option value="capacity">Capacity</option>
+    <option value="bookable">Bookable</option>
+  </select> 
+<input type="text" placeholder="type new value here.." name="updateValueData" size="18">
+<p><font size="3">Identify the location ID of which you want to change the above value for :</p>
+<input type="text" placeholder="type location ID here.." name="updateValueDataID" size="18">
+<input type="submit" name="updateValueAction" value="Update">
+
+</form>
+
+<p><font size="3">Search for a location with at least the indicated capacity allowed :</p>
+<form method="POST" action="location.php"> 
+    <select name="updateValueCapacity">
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="20">20</option>
+        <option value="40">40</option>
+    </select> 
+   <p><input type="submit" value="Search" name="locationCapacitySearch"></p>
+</form>
+
+
+
+<p><font size="3">Insert a new location info into our Location database table below, for Bookable input "Y" for yes and "N" for no:</p>
+
+<form method="POST" action="location.php">
+<!-- refreshes page when submitted -->
+
+   <p>
+    <input type="text" placeholder="Location ID"name="insID" size="18">
+    <input type="text" placeholder="Building Code" name="insBuildingCode" size="18">
+    <input type="text" placeholder="Area Code" name="insAreaCode" size="18">
+    <input type="number" placeholder="Capacity"name="insCapacity" size="18">
+    <input type="text" placeholder="Bookable" name="insBookable" size="1">
+<!-- Define two variables to pass values. -->    
+<input type="submit" value="Insert Location Data" name="insertsubmit"></p>
+<input type="submit" value="Clear Location Database" name="deleteAll"></p>
+<input type="submit" value="See All Records" name="seeAll">
+</form>
+
+<!-- Create a form to pass the values.  
+     See below for how to get the values. --> 
+
+
+<html>
+<style>
+    table {
+        width: 20%;
+        border: 1px solid black;
+    }
+
+    form {
+        margin-bottom: 60px;
+    }
+
+    th {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: .7em;
+        background: #666;
+        color: #FFF;
+        padding: 2px 6px;
+        border-collapse: separate;
+        border: 1px solid #000;
+    }
+
+    td {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: .7em;
+        border: 1px solid #DDD;
+        color: black;
+    }
+
+    ::placeholder {
+    color: gray;
+    opacity: 0.65; /* Firefox */
+    }
+
+    :-ms-input-placeholder { /* Internet Explorer 10-11 */
+    color: red;
+    }
+
+    ::-ms-input-placeholder { /* Microsoft Edge */
+    color: red;
+    }
+</style>
+</html>
+
+
+
+<?php
+
+//See all location listings 
+//Search locations by name, input textbox
+
+/* This tells the system that it's no longer just parsing 
+   HTML; it's now parsing PHP. */
+
+// keep track of errors so it redirects the page only if
+// there are no errors
+
+
+$localvarrr = 3;
+$success = True;
+$db_conn = OCILogon("ora_ansel", "a15984164", 
+                    "dbhost.students.cs.ubc.ca:1522/stu");
+
+function executePlainSQL($cmdstr) { 
+     // Take a plain (no bound variables) SQL command and execute it.
+	//echo "<br>running ".$cmdstr."<br>";
+	global $db_conn, $success;
+	$statement = OCIParse($db_conn, $cmdstr); 
+     // There is a set of comments at the end of the file that 
+     // describes some of the OCI specific functions and how they work.
+
+	if (!$statement) {
+		echo "<br>Cannot parse this command: " . $cmdstr . "<br>";
+		$e = OCI_Error($db_conn); 
+           // For OCIParse errors, pass the connection handle.
+		echo htmlentities($e['message']);
+		$success = False;
+	}
+
+	$r = OCIExecute($statement, OCI_DEFAULT);
+	if (!$r) {
+		echo "<br>Cannot execute this command: " . $cmdstr . "<br>";
+		$e = oci_error($statement); 
+           // For OCIExecute errors, pass the statement handle.
+		echo htmlentities($e['message']);
+		$success = False;
+	} else {
+
+	}
+	return $statement;
+
+}
+
+function debug_to_console($data) {
+    $output = $data;
+    if ( is_array( $output ) )
+        $output = implode( ',', $output);
+
+    echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
+}
+
+function executeBoundSQL($cmdstr, $list) {
+	/* Sometimes the same statement will be executed several times.
+        Only the value of variables need to be changed.
+	   In this case, you don't need to create the statement several
+        times.  Using bind variables can make the statement be shared
+        and just parsed once.
+        This is also very useful in protecting against SQL injection
+        attacks.  See the sample code below for how this function is
+        used. */
+
+	global $db_conn, $success;
+	$statement = OCIParse($db_conn, $cmdstr);
+
+	if (!$statement) {
+		echo "<br>Cannot parse this command: " . $cmdstr . "<br>";
+		$e = OCI_Error($db_conn);
+		echo htmlentities($e['message']);
+		$success = False;
+	}
+
+	foreach ($list as $tuple) {
+		foreach ($tuple as $bind => $val) {
+			//echo $val;
+			//echo "<br>".$bind."<br>";
+			OCIBindByName($statement, $bind, $val);
+			unset ($val); // Make sure you do not remove this.
+                              // Otherwise, $val will remain in an 
+                              // array object wrapper which will not 
+                              // be recognized by Oracle as a proper
+                              // datatype.
+		}
+		$r = OCIExecute($statement, OCI_DEFAULT);
+		if (!$r) {
+			echo "<br>Cannot execute this command: " . $cmdstr . "<br>";
+			$e = OCI_Error($statement);
+                // For OCIExecute errors pass the statement handle
+			echo htmlentities($e['message']);
+			echo "<br>";
+			$success = False;
+		}
+	}
+
+}
+
+function printTable($resultFromSQL, $namesOfColumnsArray)
+{
+        echo "<br>Here is the output, nicely formatted:<br>";
+        echo "<table>";
+        echo "<tr>";
+        // iterate through the array and print the string contents
+        foreach ($namesOfColumnsArray as $name) {
+            echo "<th>$name</th>";
+        }
+        echo "</tr>";
+
+        while ($row = OCI_Fetch_Array($resultFromSQL, OCI_BOTH)) {
+            echo "<tr>";
+            $string = "";
+
+            // iterates through the results returned from SQL query and
+            // creates the contents of the table
+            for ($i = 0; $i < sizeof($namesOfColumnsArray); $i++) {
+                $string .= "<td>" . $row["$i"] . "</td>";
+            }
+            echo $string;
+            echo "</tr>";
+        }
+        echo "</table>";
+}
+
+
+
+// Connect Oracle...
+if ($db_conn) {
+    global $localvarrr;
+	if (array_key_exists('reset', $_POST)) {
+		// Drop old table...
+		echo "<br> dropping table <br>";
+		executePlainSQL("Drop table location");
+
+		// Create new table...
+		echo "<br> creating new table <br>";
+		executePlainSQL("create table location (locationID varchar2(30), buildingCode varchar2(30), areaCode varchar2(30), capacity number, bookable varchar2(1), primary key (locationID))");
+        OCICommit($db_conn);
+
+	} else {
+		if (array_key_exists('insertsubmit', $_POST)) {
+            $localvarrr = 6;
+			// Get values from the user and insert data into 
+                // the table.
+			$tuple = array (
+                ":bind1" => $_POST['insID'],
+                ":bind2" => $_POST['insBuildingCode'],
+                ":bind3" => $_POST['insAreaCode'],
+                ":bind4" => $_POST['insCapacity'],
+                ":bind5" => $_POST['insBookable']
+                
+			);
+			$alltuples = array (
+				$tuple
+			);
+			executeBoundSQL("insert into location values (:bind1, :bind2, :bind3, :bind4, :bind5)", $alltuples);
+			OCICommit($db_conn);
+
+        }
+        else {
+            if (array_key_exists('deleteAll', $_POST)) {
+                executePlainSQL("delete from location");
+                OCICommit($db_conn);
+            } 
+            else {
+                if (array_key_exists('updateValueAction', $_POST) || array_key_exists('updateValue', $_POST)) {
+                    $tuple = array (
+                        ":bind1" => $_POST['updateValueData'],
+                        ":bind2" => $_POST['updateValue'],
+                        ":bind3" => $_POST['updateValueDataID']
+                    );
+                    $alltuples = array (
+                        $tuple
+                    );
+                    executeBoundSQL("update location set " . $_POST['updateValue'] . "=:bind1 where locationID=:bind3 ", $alltuples);
+                    OCICommit($db_conn);
+                }
+            }
+        } 
+    }
+    $lol = array_key_exists('locationSearch', $_POST) || array_key_exists('locationCapacitySearch', $_POST);
+    $lol = !$lol;
+	if ($_POST && $success && $lol) {
+        //POST-REDIRECT-GET -- See http://en.wikipedia.org/wiki/Post/Redirect/Get
+        header("location: location.php");
+	} else {
+        // Select data...
+        if (array_key_exists('locationSearch', $_POST)) {
+            $eventsearched = $_POST['locationSearchString'];
+            $result = executePlainSQL("select * from location where locationID like '%" . $eventsearched . "%'");
+        } elseif (array_key_exists('locationCapacitySearch', $_POST)) {
+            $CapacitySearched = $_POST['updateValueCapacity'];
+            $result = executePlainSQL("select * from location where capacity >= " . $CapacitySearched . "");
+        } else {
+            $result = executePlainSQL("select * from location");
+        }
+        $columnNames = array("Location ID", "Building Code", "Area Code", "Capacity", "Bookable");
+        printTable($result, $columnNames);
+	}
+
+	//Commit to save changes...
+    OCILogoff($db_conn);
+
+
+} else {
+	echo "cannot connect";
+	$e = OCI_Error(); // For OCILogon errors pass no handle
+	echo htmlentities($e['message']);
+}
--- main.php
+++ main.php
@@ -1,4 +1,8 @@
 <p><font size="6">Welcome to the AMS all inclusive system, make your menu selection below</p>
 <p><font size ="4.5">Student Profile: </p><a href="studentView.php"><font size= "3">Click Here to see your student profile as well as update any information</a><br/>
-<p><font size ="4.5">Events: </p><a href="eventView.php"><font size= "3">Click Here to see all events</a><br/>
+<p><font size ="4.5">Events: </p><a href="eventView.php"><font size= "3">Click Here to see all AMS events</a><br/>
+<p><font size ="4.5">AMS Executive Team: </p><a href="executiveView.php"><font size= "3">Click Here to get to know we students who work behind-the-scenes as part of the AMS!</a><br/>
 <p><font size ="4.5">Clubs: </p><a href="clubView.php"><font size= "3">Click Here to see all available clubs and their locations</a><br/>
+<p><font size ="4.5">Resources: </p><a href="resourceView.php"><font size= "3">Click Here to see all AMS resources</a><br/>
+<p><font size ="4.5">Business: </p><a href="businessView.php"><font size= "3">Click Here to see all AMS-run businesses in UBC</a><br/>
+<p><font size ="4.5">Locations: </p><a href="locationView.php"><font size= "3">Click Here to view all AMS owned locations around campus</a><br/>
